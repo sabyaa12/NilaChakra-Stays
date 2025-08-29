38d18e4d0d0e77b1dc9531591c96066747da8f81
sabyasachi40 <b322040@iiit-bh.ac.in>
2025-08-30 03:24:17 +0530

    fix: update package.json and middleware null checks

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,83 +1,89 @@
 const { models } = require("mongoose");
 const Listing = require("../models/listing.js");
- // require from the listing.js file from models folder 
 const axios = require("axios");
 const ExpressError = require("../utils/ExpressError");
 
-module.exports.index = async (req, res) => { // index ke liye 
+module.exports.index = async (req, res) => {
     const allListing = await Listing.find({});
     res.render("listings/index.ejs", { allListing });
 };
 
-module.exports.renderNewForm = (req, res) => { 
-   //console.log("this is the details : " ,req.user);
+module.exports.renderNewForm = (req, res) => {
     res.render("listings/new.ejs");
 };
 
 module.exports.showListing = async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id)
-    .populate({path:"reviews",
-        populate :{
-            path: "author",
-        },
-    }).populate("owner"); // ID ke basis pe find karenge 
-    if(!listing){
-        req.flash("error" , "listing you requested for does not exist!");
-        res.redirect("/listings");
+        .populate({
+            path: "reviews",
+            populate: {
+                path: "author",
+            },
+        }).populate("owner");
+    
+    if (!listing) {
+        req.flash("error", "listing you requested for does not exist!");
+        return res.redirect("/listings");
     }
-    console.log(listing); 
-    res.render("listings/show.ejs", { listing , mapApiKey: process.env.MAP_API_KEY});
-    // res.render("listings/show", { listing,  });
+    
+    console.log(listing);
+    res.render("listings/show.ejs", { listing, mapApiKey: process.env.MAP_API_KEY });
 };
 
 module.exports.createListing = async (req, res, next) => {
-        //SCHEMA WALA PART 
     let url = req.file.path;
     let filename = req.file.filename;
-   // let listing = new Listing(req.body.listing);
-    // console.log(url , ".." , filename);
-const newListing = new Listing(req.body.listing);
-newListing.owner = req.user._id;
-newListing.image = {url , filename};
-console.log("Creating listing for location:", newListing.location);
-console.log("MAP API KEY is: ", process.env.MAP_API_KEY);
-   let fetchUrl = `https://api.olamaps.io/places/v1/geocode?address=${newListing.location}&language=English&api_key=${process.env.MAP_API_KEY}`;
-    let data = await axios.get(fetchUrl);  
+    
+    const newListing = new Listing(req.body.listing);
+    newListing.owner = req.user._id;
+    newListing.image = { url, filename };
+    
+    console.log("Creating listing for location:", newListing.location);
+    console.log("MAP API KEY is: ", process.env.MAP_API_KEY);
+    
+    let fetchUrl = `https://api.olamaps.io/places/v1/geocode?address=${newListing.location}&language=English&api_key=${process.env.MAP_API_KEY}`;
+    let data = await axios.get(fetchUrl);
     let { lng, lat } = data.data.geocodingResults[0].geometry.location;
     newListing.geometry = { type: "Point", coordinates: [lng, lat] };
-
+    
     await newListing.save();
-
-    req.flash("success", "New Listing created" );
+    
+    req.flash("success", "New Listing created");
     res.redirect("/listings");
-}; 
+};
+
 module.exports.renderEditForm = async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-     if(!listing){
-        req.flash("error" , "listing you requested for does not exist!");
-        res.redirect("/listings");
+    
+    if (!listing) {
+        req.flash("error", "listing you requested for does not exist!");
+        return res.redirect("/listings");
     }
-    let originalImageUrl = listing.image.url ;
-originalImageUrl = originalImageUrl.replace("/upload" ,"/upload/h_300,w_250");
-    res.render("listings/edit.ejs", { listing , originalImageUrl});
+    
+    let originalImageUrl = listing.image.url;
+    originalImageUrl = originalImageUrl.replace("/upload", "/upload/h_300,w_250");
+    res.render("listings/edit.ejs", { listing, originalImageUrl });
 };
 
 module.exports.updateListing = async (req, res) => {
-    // if (!req.body.listing) {
-    //     throw new ExpressError(400, " senda valid data for listing");
-    // }
     let { id } = req.params;
     let listing = await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-    if(typeof req.file !== "undefined"){ // condition req.file undefined to nahi hai , (matlab exist karta hai ya nahi)
-    let url = req.file.path;
-    let filename = req.file.filename;
-    listing.image = { url , filename};
-    await listing.save();
+    
+    if (!listing) {
+        req.flash("error", "listing you requested for does not exist!");
+        return res.redirect("/listings");
     }
-
-    req.flash("success", "Listing Updated!" );
+    
+    if (typeof req.file !== "undefined") {
+        let url = req.file.path;
+        let filename = req.file.filename;
+        listing.image = { url, filename };
+        await listing.save();
+    }
+    
+    req.flash("success", "Listing Updated!");
     res.redirect(`/listings/${id}`);
 };
 
@@ -85,6 +91,6 @@ module.exports.destroyListing = async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
-    req.flash("success", " Listing deleted! " );
+    req.flash("success", " Listing deleted! ");
     res.redirect("/listings");
 };
--- middleware.js
+++ middleware.js
@@ -2,10 +2,10 @@ const Listing = require("./models/listing");
 const Review = require("./models/review.js");
 const {listingSchema , reviewSchema } = require("./schema.js");
 const ExpressError = require("./utils/ExpressError.js");
+
 module.exports.isLoggedIn = (req, res , next) => {
-   // console.log(req.path , ".." , req.originalUrl);
     if(!req.isAuthenticated()) {
-       req.session.redirectUrl = req.originalUrl ; // redirectUrl ko save 
+       req.session.redirectUrl = req.originalUrl;
         req.flash("error" , "you must be logged in to create listing") ;
         return res.redirect("/login");
     }
@@ -21,44 +21,54 @@ module.exports.saveRedirectUrl = (req,res,next) =>{
 
 module.exports.isOwner = async (req,res,next) => {
     let { id } = req.params;
-   let listing = await Listing.findById(id);
-   if(!listing.owner._id.equals(res.locals.currUser._id)){
-    req.flash("error" , "you are not the owner of this page!");
-    return res.redirect(`/listings/${id}`);
-   }
-
-   next();
+    let listing = await Listing.findById(id);
+    
+    // ADD THIS NULL CHECK - THIS IS THE MISSING PIECE!
+    if(!listing) {
+        req.flash("error" , "listing you requested for does not exist!");
+        return res.redirect("/listings");
+    }
+    
+    if(!listing.owner._id.equals(res.locals.currUser._id)){
+        req.flash("error" , "you are not the owner of this page!");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
 };
 
 module.exports.validatelisting = (req,res,next) => {
-
-let {error} =  listingSchema.validate(req.body);
-   if(error){
-    let errorMsg = error.details.map((el)=> el.message) .join(",");
-    throw new ExpressError(400 , errorMsg);
-   } else {
-    next();
-   }
-
+    let {error} =  listingSchema.validate(req.body);
+    if(error){
+        let errorMsg = error.details.map((el)=> el.message).join(",");
+        throw new ExpressError(400 , errorMsg);
+    } else {
+        next();
+    }
 };
 
 module.exports.validateReview = (req , res , next) => {
-let {error} =  reviewSchema.validate(req.body);
-   if(error){
-    let errorMsg = error.details.map((el)=> el.message) .join(",");
-    throw new ExpressError(400 , errorMsg);
-   } else {
-    next();
-   } 
+    let {error} =  reviewSchema.validate(req.body);
+    if(error){
+        let errorMsg = error.details.map((el)=> el.message).join(",");
+        throw new ExpressError(400 , errorMsg);
+    } else {
+        next();
+    } 
 };
- 
+
 module.exports.isReviewAuthor = async (req,res,next) => {
     let { id , reviewId } = req.params;
-   let review = await Review.findById(reviewId);
-   if(!review.author.equals(res.locals.currUser._id)){
-    req.flash("error" , "you are not the author of this review!");
-    return res.redirect(`/listings/${id}`);
-   }
-
-   next();
+    let review = await Review.findById(reviewId);
+    
+    // ADD THIS NULL CHECK - THIS IS THE MISSING PIECE!
+    if(!review) {
+        req.flash("error" , "review you requested for does not exist!");
+        return res.redirect(`/listings/${id}`);
+    }
+    
+    if(!review.author.equals(res.locals.currUser._id)){
+        req.flash("error" , "you are not the author of this review!");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
 };
